769770cbfaac2315f0506ff979200c6ecca4de74
Prashant Srinivasan <sivankoilprashant@gmail.com>
2020-05-08 23:37:16 -0700

    checking for anagram using sorting and position checking. Include test for main.

--- anagram-checking.py
+++ anagram-checking.py
@@ -1,19 +1,25 @@
 # The goal of this function is to take two strings and return whether they are anagrams.
 
+
 def anagram_checker(word1,word2):
     sorted_word1 = list(word1)
     sorted_word2 = list(word2)
 
-    alist1.sort()
-    alist2.sort()
+    # Sort the lists so we can compare. Sorting can be done in O(nlogn) complexity.
+    sorted_word1.sort()
+    sorted_word2.sort()
 
     pos = 0
     matches = True
 
-    while pos < len(s1) and matches:
-        if alist1[pos]==alist2[pos]:
+    while pos < len(word1) and matches:
+        if sorted_word1[pos] == sorted_word2[pos]:
             pos = pos + 1
         else:
             matches = False
 
     return matches
+
+
+if __name__ == "__main__":
+    print(anagram_checker('abcde1', 'edcba'))
